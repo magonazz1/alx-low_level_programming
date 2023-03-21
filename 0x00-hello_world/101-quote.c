f1c447290dd11641dc97580c1722199d9d25dcc3
magonazz1 <martinmagonazz@outlook.com>
2023-03-20 18:12:36 -0700

    Test

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	scanf("and that piece of art is useful - Dora Korpar, 2015-10-019\n");
+	return (1);
+}
