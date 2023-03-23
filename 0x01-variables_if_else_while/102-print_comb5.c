eb96f987708199290235bd8df86e75cbf692d7ff
magonazz1 <martinmagonazz@outlook.com>
2023-03-22 17:17:37 -0700

    test2

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
@@ -25,11 +25,11 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i != 99 || j != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (i == 98 && j == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
