47a54481c30ff82ccb1062a6c7ddf97cc80139f4
magonazz1 <martinmagonazz@outlook.com>
2023-03-22 18:12:42 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints -putchar followed by a new line
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
