f99bd4151d209a6f6558421ecdea656ab3e80519
magonazz1 <martinmagonazz@outlook.com>
2023-04-24 19:13:00 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
- * struct dog - describes a dog
- *
- * @name: dog name
- *
- * @age: dog age
- *
- * @owner: dog owner
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
 struct dog
 {
@@ -15,14 +13,47 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
- * dog_t - struct dog (typedef)
+ * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
+/**
+ * init_dog - Initializes a variable of type struct dog.
+ * @d: The dog to be initialized.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: void
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - Prints a struct dog.
+ * @d: The struct dog to be printed.
+ *
+ * Return: void
+ */
 void print_dog(struct dog *d);
+
+/**
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: A pointer to the new struct dog.
+ */
 dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * free_dog - Frees memory allocated for a struct dog.
+ * @d: The struct dog to be freed.
+ *
+ * Return: void
+ */
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
