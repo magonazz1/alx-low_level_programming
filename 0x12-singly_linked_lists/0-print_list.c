88c9cbc047ba412547e849b4a11b7d7b308d1581
magonazz1 <martinmagonazz@outlook.com>
2023-04-27 17:06:48 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - function that prints all elements of list_t
+ *
+ * @h: pointer to the head of list_t
+ *
+ * Return: nodes in the list_t
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	size_t node = 0;
+
+	if (current == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (current != NULL)
+		{
+			if (current->str != NULL)
+			{
+				printf("[%u] %s\n", current->len, current->str);
+			}
+			else
+			{
+				printf("[0] (nil\n");
+			}
+			current = current->next;
+			node++;
+		}
+		return (node);
+	}
+}
