d540ae3f0e438ba57d226a7614a7d1a27e434bee
magonazz1 <martinmagonazz@outlook.com>
2023-04-27 19:48:30 -0700

    6. Real programmers can write assembly code in any language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,27 @@
+; Start of the program
+; Description: 64-bit program in assembly that
+; prints Hello, Holberton, followed by a new line.
+
+extern printf
+
+section .text
+	global main
+
+main:
+	push rbp
+
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
+
+	pop rbp
+
+	mov rax,0
+	ret
+
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
+
+;end of the program
