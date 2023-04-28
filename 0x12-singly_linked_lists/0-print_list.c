8d2334ed933cbfb7e7ea400415c95417d1cba548
magonazz1 <martinmagonazz@outlook.com>
2023-04-27 17:25:02 -0700

    0. Printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,21 +11,20 @@
  */
 size_t print_list(const list_t *h)
 {
-	const list_t *current = h;
-	size_t node = 0;
+	size_t count = 0;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str != NULL)
+		if (h->str != NULL)
 		{
-			printf("[%u] %s\n", current->len, current->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		else
 		{
 			printf("[0] (nil\n");
 		}
-		node++;
-		current = current->next;
+		h = h->next;
+		count++;
 	}
-	return (node);
+	return (count);
 }
