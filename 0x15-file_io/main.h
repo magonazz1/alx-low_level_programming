b4d51da14e10c4e0a1bd5889fce3728093fba474
magonazz1 <martinmagonazz@outlook.com>
2023-05-08 18:41:30 -0700

    1. Under the snow

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
