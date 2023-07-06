8dc4d29397ab8ab4ffa723f104b089fdc3f57879
magonazz1 <martinmagonazz@outlook.com>
2023-07-06 13:22:31 -0700

    1.1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int _putchar(char c);
+
+/**
+ * print_binary - function that prints binary representations of a number
+ *
+ * @n: the number to be printed
+ *
+ * Return: Void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int pack = 1;
+	int x = 0;
+
+	for (; x < 63; x++)
+	{
+		pack <<= 1;
+		if (pack > n)
+		{
+			break;
+		}
+	}
+	for (; x >= 0; x--)
+	{
+		if ((n >> x) & 1)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+	}
+}
