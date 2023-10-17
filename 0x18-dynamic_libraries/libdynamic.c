9f6ed42769023fbcff3b954ddefea2cb6d497f02
magonazz1 <martinmagonazz@outlook.com>
2023-10-16 17:26:50 -0700

    first dynamiclib

---
+++ 0x18-dynamic_libraries/libdynamic.c
@@ -0,0 +1,238 @@
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to the standard output.
+ * @c: The character to write.
+ *
+ * Return: The number of characters written (1).
+ */
+int _putchar(char c)
+{
+	/* Some code */
+}
+
+/**
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+        /* Some code */
+}
+
+/**
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is alphabetic, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+        /* Some code */
+}
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to compute the absolute value for.
+ *
+ * Return: The absolute value of the integer.
+ */
+int _abs(int n)
+{
+        /* Some code */
+}
+
+/**
+ * _isupper - Checks if a character is uppercase.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+        /* Some code */
+}
+
+/**
+ * _isdigit - Checks if a character is a digit.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+        /* Some code */
+}
+
+/**
+ * _strlen - Computes the length of a string.
+ * @s: The string to compute the length for.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+        /* Some code */
+}
+
+/**
+ * _puts - Writes a string to the standard output.
+ * @s: The string to write.
+ */
+void _puts(char *s)
+{
+        /* Some code */
+}
+
+/**
+ * _strcpy - Copies a string.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ *
+ * Return: The destination buffer (dest).
+ */
+char *_strcpy(char *dest, char *src)
+{
+        /* Some code */
+}
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+        /* Some code */
+}
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string.
+ * @src: The source string to append.
+ *
+ * Return: The concatenated string (dest).
+ */
+char *_strcat(char *dest, char *src)
+{
+        /* Some code */
+}
+
+/**
+ * _strncat - Concatenates n characters from two strings.
+ * @dest: The destination string.
+ * @src: The source string to append.
+ * @n: The maximum number of characters to concatenate.
+ *
+ * Return: The concatenated string (dest).
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        /* Some code */
+}
+
+/**
+ * _strncpy - Copies n characters from a string.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ * @n: The number of characters to copy.
+ *
+ * Return: The destination buffer (dest).
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+        /* Some code */
+}
+
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ *
+ * Return: An integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2.
+ */
+int _strcmp(char *s1, char *s2)
+{
+        /* Some code */
+}
+
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: The memory area to fill.
+ * @b: The constant byte to fill.
+ * @n: The number of bytes to fill.
+ *
+ * Return: A pointer to the filled memory area (s).
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+        /* Some code */
+}
+
+/**
+ * _memcpy - Copies memory area from src to dest.
+ * @dest: The destination memory area.
+ * @src: The source memory area.
+ * @n: The number of bytes to copy.
+ *
+ * Return: A pointer to the destination memory area (dest).
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        /* Some code */
+}
+
+/**
+ * _strchr - Locates the first occurrence of a character in a string.
+ * @s: The string to search.
+ * @c: The character to locate.
+ *
+ * Return: A pointer to the first occurrence of c in s or NULL if not found.
+ */
+char *_strchr(char *s, char c)
+{
+        /* Some code */
+}
+
+/**
+ * _strspn - Calculates the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The string containing characters to match.
+ *
+ * Return: The number of bytes in s that consist of characters found in accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+        /* Some code */
+}
+
+/**
+ * _strpbrk - Searches a string for any of a set of bytes.
+ * @s: The string to be searched.
+ * @accept: The string containing characters to search for.
+ *
+ * Return: A pointer to the character in s that matches one of the characters
+ *		in accept or NULL if none is found.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+        /* Some code */
+}
+
+/**
+ * _strstr - Locates a substring within a string.
+ * @haystack: The string to search within.
+ * @needle: The substring to search for.
+ *
+ * Return: A pointer to the beginning of the located substring in haystack
+ *		or NULL if not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+        /* Some code */
+}
+
