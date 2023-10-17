6fe32eab79768eedade55740b89c1ecf857f13b5
magonazz1 <martinmagonazz@outlook.com>
2023-10-16 17:59:33 -0700

    3rd task

---
+++ 0x18-dynamic_libraries/math_funcs.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the addition (a + b).
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the subtraction (a - b).
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the multiplication (a * b).
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Divides two integers.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The result of the division (a / b).
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - Computes the remainder of the division of two integers.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The remainder of the division (a % b).
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
